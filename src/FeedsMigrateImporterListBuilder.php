82c927ecd7d9da3efde847309d362cd53cc7e2e9
Mike Decker <pookmish@gmail.com>
2018-04-20 22:31:55 -0700

    displayed last imported in list

--- src/FeedsMigrateImporterListBuilder.php
+++ src/FeedsMigrateImporterListBuilder.php
@@ -2,10 +2,15 @@
 
 namespace Drupal\feeds_migrate;
 
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
+use Drupal\Core\Datetime\DateFormatterInterface;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\migrate\MigrateMessage;
 use Drupal\migrate_plus\Entity\Migration;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class FeedsMigrateImporterListBuilder.
@@ -14,12 +19,38 @@ use Drupal\migrate_plus\Entity\Migration;
  */
 class FeedsMigrateImporterListBuilder extends ConfigEntityListBuilder {
 
+  protected $dateFormatter;
+
+  protected $dateTime;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity.manager')->getStorage($entity_type->id()),
+      $container->get('date.formatter'),
+      $container->get('datetime.time')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, DateFormatterInterface $date_formatter, TimeInterface $time) {
+    parent::__construct($entity_type, $storage);
+    $this->dateFormatter = $date_formatter;
+    $this->dateTime = $time;
+  }
+
   /**
    * {@inheritdoc}
    */
   public function buildHeader() {
     $header['label'] = $this->t('Importer');
     $header['migrator'] = $this->t('Migration');
+    $header['last'] = $this->t('Last Imported');
     return $header + parent::buildHeader();
   }
 
@@ -32,6 +63,11 @@ class FeedsMigrateImporterListBuilder extends ConfigEntityListBuilder {
 
     $data['label'] = $entity->label();
     $data['migrator'] = $migration->label();
+
+    $data['last'] = $this->t('Never');
+    if ($entity->lastRan) {
+      $data['last'] = $this->dateFormatter->formatDiff($entity->lastRan, $this->dateTime->getRequestTime(), ['granularity' => 1]);
+    }
 
     $row = [
       'class' => $entity->status() ? 'enabled' : 'disabled',
